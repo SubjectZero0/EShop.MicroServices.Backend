50b7f7a21f13a30c4320c5ed7df3ecec7e5ae32d
Alexandrer Iordanou <iordanoualexander@gmail.com>
2024-06-03 19:48:21 +0300

    Project Setup

--- EShop.MicroServices.Backend/Orchestration/EShop.MicroServices.AppHost/Program.cs
+++ EShop.MicroServices.Backend/Orchestration/EShop.MicroServices.AppHost/Program.cs
@@ -1,11 +1,24 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var catalogDb = builder.AddPostgres("postgres-eshop")
+var postgres = builder.AddPostgres("postgres-eshop");
+
+//-------------------------------------------------------------------
+
+var catalogDb = postgres
 	.WithEnvironment("POSTGRES_DB", "CatalogDb")
 	.AddDatabase("CatalogDb", "CatalogDb");
 
 builder.AddProject<Projects.Catalog_Api>("catalog-api")
 	.WithReference(catalogDb)
 	.WithReplicas(2);
+
+//-------------------------------------------------------------------
+
+var basketDb = postgres
+	.WithEnvironment("POSTGRES_DB", "BasketDb")
+	.AddDatabase("BasketDb", "BasketDb");
+
+builder.AddProject<Projects.Basket_Api>("basket-api")
+	.WithReference(basketDb);
 
 builder.Build().Run();
---
+++ EShop.MicroServices.Backend/Services/Basket/Basket.Api/Program.AddCarterEndpoints.cs
@@ -0,0 +1,17 @@
+﻿using Carter;
+
+namespace Basket.Api
+{
+	public static partial class Program
+	{
+		public static WebApplicationBuilder AddCarterEndpoints(this WebApplicationBuilder builder)
+		{
+			builder.Services.AddCarter(configurator: cfg =>
+			{
+				//cfg.WithModule<>();
+			});
+
+			return builder;
+		}
+	}
+}

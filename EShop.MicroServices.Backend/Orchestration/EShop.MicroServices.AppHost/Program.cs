772ed92d1d29a1043218aadfeb61b8f82f87bd0b
Alexandrer Iordanou <iordanoualexander@gmail.com>
2024-06-03 20:17:40 +0300

    Add replicas for services and change default Basket.Api ports

--- EShop.MicroServices.Backend/Orchestration/EShop.MicroServices.AppHost/Program.cs
+++ EShop.MicroServices.Backend/Orchestration/EShop.MicroServices.AppHost/Program.cs
@@ -8,17 +8,19 @@ var catalogDb = postgres
 	.WithEnvironment("POSTGRES_DB", "CatalogDb")
 	.AddDatabase("CatalogDb", "CatalogDb");
 
+// Add two instances of Catalog.Api
 builder.AddProject<Projects.Catalog_Api>("catalog-api")
 	.WithReference(catalogDb)
 	.WithReplicas(2);
-
 //-------------------------------------------------------------------
 
 var basketDb = postgres
 	.WithEnvironment("POSTGRES_DB", "BasketDb")
 	.AddDatabase("BasketDb", "BasketDb");
 
+// Add two instances of Basket.Api
 builder.AddProject<Projects.Basket_Api>("basket-api")
-	.WithReference(basketDb);
+	.WithReference(basketDb)
+	.WithReplicas(2);
 
 builder.Build().Run();

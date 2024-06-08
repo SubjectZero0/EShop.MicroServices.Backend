1137f18193d56d0abe67c0edfbd5d5ffc4e3c9b2
SubjectZero0 <iordanoualexander@gmail.com>
2024-06-08 19:48:29 +0300

    Add Basket Domain

---
+++ EShop.MicroServices.Backend/Services/Basket/Basket.Domain/Aggregates/ShoppingCarts/ShoppingCart.cs
@@ -0,0 +1,59 @@
+using System.Text.Json.Serialization;
+
+namespace Basket.Domain.Aggregates.ShoppingCarts;
+
+public class ShoppingCart : AggregateRoot<Guid>
+{
+    [JsonInclude]
+    public string UserName { get; }
+
+    [JsonInclude]
+    public List<ShoppingCartItem> Items { get; private set; }
+
+    [JsonInclude]
+    public DateTime CreatedAt { get; }
+    
+    [JsonInclude]
+    public DateTime UpdatedAt { get; private set; }
+    
+    [JsonInclude]
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public ShoppingCart() : base(default) { }
+
+    public ShoppingCart(Guid id, string userName, List<ShoppingCartItem> items, DateTime timeStamp) : base(id)
+    {
+        UserName = userName;
+        Items = items;
+        CreatedAt = timeStamp;
+        UpdatedAt = timeStamp;
+    }
+
+    public static ShoppingCart CreateNew(string userName, List<ShoppingCartItem> items, DateTime timeStamp)
+    {
+        return new ShoppingCart(
+            id: Guid.NewGuid(),
+            userName: userName,
+            items: items,
+            timeStamp: timeStamp);
+    }
+
+    public void AddShoppingCartItem(ShoppingCartItem item, DateTime timeStamp)
+    {
+        Items.Add(item);
+        UpdatedAt = timeStamp;
+    }
+
+    public void UpdateItems(ShoppingCartItem[] items)
+    {
+        foreach (var item in items)
+        {
+            var updatedItem = Items.FirstOrDefault(cartItem => cartItem.ProductId == item.ProductId);
+            
+            if (updatedItem is null)
+                continue;
+            
+            updatedItem.Update(item.Quantity);
+        }
+    }
+}
---
+++ EShop.MicroServices.Backend/Services/Basket/Basket.Domain/Aggregates/ShoppingCarts/ShoppingCartItem.cs
@@ -0,0 +1,49 @@
+using System.Text.Json.Serialization;
+
+namespace Basket.Domain.Aggregates.ShoppingCarts;
+
+public class ShoppingCartItem : ValueObject
+{
+    [JsonInclude]
+    public int Quantity { get; private set; } 
+    
+    [JsonInclude]
+    public string Color { get; }
+    
+    [JsonInclude]
+    public decimal Price { get; }
+    
+    [JsonInclude]
+    public Guid ProductId { get; }
+    
+    [JsonInclude]
+    public string ProductName { get; }
+
+    public ShoppingCartItem()
+    {
+    }
+
+    public ShoppingCartItem(int quantity, string color, decimal price, Guid productId, string productName)
+    {
+        Quantity = quantity;
+        Color = color;
+        Price = price;
+        ProductId = productId;
+        ProductName = productName;
+    }
+
+    public static ShoppingCartItem CreateNew(int quantity, string color, decimal price, Guid productId, string productName)
+    {
+        return new ShoppingCartItem(
+            quantity: quantity,
+            color: color,
+            price: price,
+            productId: productId,
+            productName: productName);
+    }
+
+    public void Update(int quantity)
+    {
+        Quantity = quantity;
+    }
+}

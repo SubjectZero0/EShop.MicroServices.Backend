8c5dcc724ad1964d77181ce15caa232b03142e92
SubjectZero0 <iordanoualexander@gmail.com>
2024-06-08 19:49:13 +0300

    Refactor Product domain and Configuration file

--- EShop.MicroServices.Backend/Services/Catalog/Catalog.Domain/AggregateRoot.cs
+++ EShop.MicroServices.Backend/Services/Catalog/Catalog.Domain/AggregateRoot.cs
@@ -1,7 +1,16 @@
-﻿namespace Catalog.Domain
+﻿using System.Text.Json.Serialization;
+
+namespace Catalog.Domain
 {
 	public abstract class AggregateRoot<T> where T : struct
 	{
-		public T Id { get; set; }
+		[JsonInclude]
+		public T Id { get; private set; }
+		
+		protected AggregateRoot(T id)
+		{
+			Id = id;
+		}
 	}
+	
 }
